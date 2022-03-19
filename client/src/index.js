e030c77a234e3d747c78d55ba33f5e40b133183f
vincentsurja <vincentsurja@gmail.com>
2022-03-18 18:02:58 -0700

    New branch

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// TODO: link to admin and user
 ReactDOM.render(
   <React.StrictMode>
     <App />
